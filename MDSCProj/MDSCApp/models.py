2ebe1e98063f0efc2fb4c7247c48009ba1733de9
AllenZPGu <zhepu.gu@gmail.com>
2022-06-24 15:08:40 +1000

    created guess DB

--- MDSCProj/MDSCApp/models.py
+++ MDSCProj/MDSCApp/models.py
@@ -1,3 +1,17 @@
+from django import forms
 from django.db import models
 
-# Create your models here.
+class Guess(models.Model):
+    id = models.AutoField(primary_key=True)
+    puzzle = models.IntegerField(default=0, null=True)
+    studentId = models.IntegerField(default=0, null=True)
+    name = models.CharField(max_length=200, null=True)
+    guess = models.CharField(max_length=100, null=True)
+    correct = models.BooleanField(default=False)
+    submitTime = models.DateTimeField(null=True)
+    
+    class Meta:
+        db_table = 'Guess'
+    def __str__(self):
+        output = f'{self.id}'
+        return output
--- migrate.bat
+++ migrate.bat
@@ -1,4 +1,4 @@
-CALL activate mdscenv
+CALL mdscenv/Scripts/activate
 CALL set DEVELOPMENT_MODE=True
 CALL set SECRET_KEY="ubfxhi$qrj&9$s&^g5lmru3l03h5azq&w@mfso0+*beq71x!8t"
 python MDSCProj/manage.py makemigrations
